8ab79536fcba0ec6d96fed168c1e5568b6a851d4
Dhanu Gupta <guptadhanu5842@gmail.com>
2025-06-16 21:00:34 +0530

    HyperParameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,13 +39,44 @@ class ModelTrainer:
                 "Random forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
-                "K-neighbour classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "Adaboost Classifier": AdaBoostRegressor(),
+                "KNN Regressor": KNeighborsRegressor(),
+                "XGB Regressor": XGBRegressor(),
+                "CatBoost Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            params = {
+            "Decision Tree": {
+            'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+            },
+            "Random forest": {
+            'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "Gradient Boosting": {
+            'learning_rate': [.1, .01, .05, .001],
+            'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+            'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "KNN Regressor": {
+            'n_neighbors': [3, 5, 11],
+            'weights': ['uniform', 'distance']
+            },
+            "XGB Regressor": {
+            'learning_rate': [.1, .01, .05, .001],
+            'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "CatBoost Regressor": {
+            'depth': [6, 8, 10],
+            'learning_rate': [0.01, 0.05, 0.1],
+            'iterations': [30, 50, 100]
+            },
+            "AdaBoost Regressor": {
+            'learning_rate': [.1, .01, 0.5, .001],
+            'n_estimators': [8, 16, 32, 64, 128, 256]
+            }
+        }
 
-            model_report:dict=evaluate_models(x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test,models=models)
+
+            model_report:dict=evaluate_models(x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test,models=models,param=params)
             best_model_score=max(sorted(model_report.values()))
 
             best_model_name=list(model_report.keys())[
